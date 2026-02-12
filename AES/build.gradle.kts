18f87b6c9774cb4d34369f8575abb8299db23a35
consuelita <adelita@operatorfoundation.org>
2026-02-12 15:08:33 -0600

    Lowered minSDK to 28

--- AES/build.gradle.kts
+++ AES/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36  // Changed from compileSdk { version = release(36) }
 
     defaultConfig {
-        minSdk = 33
+        minSdk = 28
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
